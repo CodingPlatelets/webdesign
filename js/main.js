bc7a2f69b08d197b68d2e6fbe2e1c11705b1933c
platelets <wenkanedward@gmail.com>
2022-10-15 21:53:58 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+/*
+ * @LastEditors: platelets wenkanedward@gmail.com
+ * @Date: 2022-10-15 15:17:48
+ * @LastEditTime: 2022-10-15 20:35:36
+ * Copyright (c) 2022 by platelets email: wenkanedward@gmail.com, All Rights Reserved.
+ */
+// load page in background
+
+let nIntervId;
+function changeBackground() {
+  if (!nIntervId) {
+    nIntervId = setInterval(changeImg, 5000);
+  }
+}
+
+const base = document.getElementById("base");
+base.style.backgroundImage = `url(./img/back/back5.jpg)`;
+window.addEventListener("load", changeBackground);
+function changeImg() {
+  base.style.backgroundImage = `url(./img/back/back${randomInt(
+    7
+  )}.jpg)`;
+}
+
+function randomInt(i) {
+  return Math.floor(Math.random() * i);
+}
+
+const startBtn = document.getElementById("startBtn");
+startBtn.addEventListener("click", () => {
+  window.location.href = "start.html";
+});
---
+++ js/start.js
@@ -0,0 +1,9 @@
+/*
+ * @LastEditors: platelets wenkanedward@gmail.com
+ * @Date: 2022-10-15 20:34:42
+ * @LastEditTime: 2022-10-15 21:07:33
+ * Copyright (c) 2022 by platelets email: wenkanedward@gmail.com, All Rights Reserved.
+ */
+
+const base = document.getElementById("base");
+base.style.backgroundImage = `url(./img/back/back0.jpg)`;
