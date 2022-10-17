75dec2cd32ac367f3cfedc8a6f4709672b4d574b
platelets <wenkanedward@gmail.com>
2022-10-17 18:23:00 +0800

    add data

--- js/cuts.js
+++ js/cuts.js
@@ -1,8 +1,38 @@
 /*
  * @LastEditors: platelets wenkanedward@gmail.com
  * @Date: 2022-10-16 11:08:13
- * @LastEditTime: 2022-10-16 11:08:45
+ * @LastEditTime: 2022-10-17 16:03:15
  * Copyright (c) 2022 by platelets email: wenkanedward@gmail.com, All Rights Reserved.
  */
-const base = document.getElementById("base");
-base.style.backgroundImage = `url(./img/back/back0.jpg)`;
+var pages = document.getElementsByClassName("page");
+for (var i = 0; i < pages.length; i++) {
+  var page = pages[i];
+  if (i % 2 === 0) {
+    page.style.zIndex = pages.length - i;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  for (var i = 0; i < pages.length; i++) {
+    //Or var page = pages[i];
+    pages[i].pageNum = i + 1;
+    pages[i].onclick = function () {
+      if (this.pageNum % 2 === 0) {
+        this.classList.remove("flipped");
+        this.previousElementSibling.classList.remove("flipped");
+      } else {
+        this.classList.add("flipped");
+        this.nextElementSibling.classList.add("flipped");
+      }
+    };
+  }
+});
+
+const page1 = document.getElementById("page1");
+page1.style.backgroundImage = `url('./img/back/back5.jpg')`;
+
+const page2 = document.getElementById("page2");
+page2.style.backgroundImage = `url('./img/back/back0.jpg')`;
+
+const page4 = document.getElementById("page4");
+page4.style.backgroundImage = `url("./img/back/back4.jpg")`;
---
+++ js/data.js
@@ -0,0 +1,4 @@
+const back = document.getElementById("backLogo");
+back.addEventListener("click", () => {
+  window.location.href = "start.html";
+});
